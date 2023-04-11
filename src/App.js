79a78eb000df5f213ccf860a485fdcc6712ccebc
Eshan <eshan.basnet@selise.ch>
2023-04-11 15:51:34 +0600

    added form and function

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
+import React,{useState,useEffect} from 'react';
 import './App.css';
 
+const initialState = {
+    username: '',
+    password: ''
+}
+
 function App() {
+    const [inputs, setInputs] = useState(initialState);
+    const [storeData, setStoreData] = useState([]);
+
+    const handleChange = (e) => {
+        const {name,value} = e.target;
+        setInputs({
+            ...inputs,
+            [name] : value
+        });
+        // console.log(inputs.username +"-"+ inputs.password)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setStoreData([...storeData, inputs]);
+        setInputs(initialState);
+    }
+
+    const removeItem = (id) => {
+        setStoreData(storeData.filter((item, index) => index !== id));
+    }
+
+    const isDisabled = inputs.username === '' || inputs.password === '';
+
+    //TO VIEW BEFOER UPDATE
+    useEffect(() => {
+        console.log(`From effect: ${inputs.username} & ${inputs.password}`);
+    }, [inputs]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section>
+        <div className="App p-4 flex flex-col justify-center items-center">
+            <h1 className={'text-center text-3xl mb-6'}>LOGIN</h1>
+            <form className={'p-8 shadow-xl'} onSubmit={handleSubmit}>
+                <div className={'text-center'}>
+                    <label>Username:</label>
+                    <input
+                        required
+                        type="text"
+                        className={'input-styles mb-4 ml-4'}
+                        placeholder={'Enter name'}
+                        name="username"
+                        value={inputs.username}
+                        onChange={handleChange}
+                    />
+                </div>
+
+                <div className={'text-center'}>
+                    <label>Password:</label>
+                    <input
+                        required
+                        type="password"
+                        className={'input-styles ml-4'}
+                        placeholder={'Enter password'}
+                        name="password"
+                        value={inputs.password}
+                        onChange={handleChange}
+                    />
+                </div>
+                <button type={"submit"} className={(isDisabled ? 'bg-gray-500 text-black cursor-not-allowed' : 'bg-red-500 text-white') +' rounded-[4px] mt-4 p-2 w-full'} disabled={isDisabled}>Login</button>
+            </form>
+            {JSON.stringify(inputs,2,null)}
+        </div>
+       <h1 className={'font-bold text-center my-6'}>LIST HERE:</h1>
+        <div className={'p-4 flex justify-center'}>
+            <ul className={'flex justify-between basis-1/2 flex-col'}>
+                {
+                    storeData.map((v,i) => (
+                        <div key={i} className={'flex justify-between w-full mb-4 pb-4 border-b-2 border-slate-500'}>
+                            <div>
+                                <li>{v.username}</li>
+                                <li>{v.password}</li>
+                            </div>
+                            <button className={'rounded p-2 bg-red-500 text-white'} onClick={()=>removeItem(i)}>x</button>
+                        </div>
+                    ))
+                }
+            </ul>
+        </div>
+    </section>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  mode: process.env.TAILWIND_MODE,
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
 ],
